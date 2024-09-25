7a9e8e2d887bd6b60dc0cc0821f68bcdc42cee18
Woo3574 <rlaxodn0429@gmail.com>
2024-09-25 21:50:55 +0900

    2024.09.25

---
+++ src/A0/연습장/PlacticeNote.java
@@ -0,0 +1,46 @@
+package A0.연습장;
+
+import java.util.Scanner;
+
+public class PlacticeNote {
+    public static void main(String[] args) {
+        condition(); // Ai조건문 연습
+    }
+
+    static void condition() {
+        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        if( num > 0) {
+//            System.out.println("양수 입니다.");
+//        }
+//        else {
+//            System.out.println("음수 입니다.");
+//        }
+//        if(num > 100) {
+//            System.out.println(num + "이 100보다 큽니다.");
+//        } else if (num < 100) {
+//            System.out.println(num + "이 100보다 작습니다.");
+//        }else {
+//            System.out.println(num + "이 100과 같습니다.");
+//        }
+
+//        * 조건문 내의 코드가 한줄이면 {} 블록스코프 생략 가능
+//        if(num > 100) System.out.println(num + "이 100보다 큽니다.");
+//        else if (num < 100) System.out.println(num + "이 100보다 작습니다.");
+//        else System.out.println(num + "이 100과 같습니다.");
+
+//        * 입력 받은 문자가 대문자인지 소문자인지 출력 하기
+//        char ch = sc.next().charAt(0); // 'A'65 'Z'90 'a' 97 'z'127
+//        if(ch >= 'a' && ch <= 'z') System.out.println(ch + "는 소문자입니다.");
+//        else if (ch >= 'A' && ch <= 'Z' ) System.out.println(ch + "는 대문자입니다.");
+//        else System.out.println(ch + "는 알파벳이 아닙니다.");
+
+//        100의 자리 정수를 입력 받아서 3개의 변수에 나누어 대입 하기
+//        354 -> a > 3 b -> 5 c ->4
+
+
+
+
+    }
+
+}
