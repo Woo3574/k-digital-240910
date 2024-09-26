3659ea26bdd82e25b3c8aa1dc3bbbd470cf286ce
Woo3574 <rlaxodn0429@gmail.com>
2024-09-26 17:32:46 +0900

    2024.09.26

--- src/Bc/영화표예매하기/MovieMain.java
+++ src/Bc/영화표예매하기/MovieMain.java
@@ -6,13 +6,35 @@ package Bc.영화표예매하기;
 // 좌석 예약 하기 : 값이 0이면 예약 안된 좌석, 1이면 판매된 좌석
 // 총 판매 금액에 대한 메서드 작성
 
+import java.util.Scanner;
+
 public class MovieMain {
     public static void main(String[] args) {
         // movieTicket 클래스에 대한 객체 생성
+        MovieTicket movieTicket = new MovieTicket(12000);
         // 입력 받기 위한 스캐너 생성
+        Scanner sc = new Scanner(System.in);
         // 메뉴 작성은 무한 반복문으로 구현
+        while (true) {
+            System.out.println("[1]예매하기");
+            System.out.println("[2]취소하기");
+            System.out.println("[3]종료하기");
+            int select = sc.nextInt();
+            switch (select) {
+                case 1:
+                    movieTicket.selectSeat();
+                    break;
+                case 2:
+                    movieTicket.cancelSeat();
+                    break;
+                case 3:
+                    System.out.println(movieTicket.totalAmount());
+                    return;
+                default:
+                    System.out.println("메뉴 선택이 잘 못 되었습니다.");
+            }
+        }
         // 1.좌석 예약 하기 메서드 호출
         // 2.종료하기 누르면 총 판매 금액 표시하고 종료
-
     }
 }
--- src/Bc/영화표예매하기/MovieTicket.java
+++ src/Bc/영화표예매하기/MovieTicket.java
@@ -1,18 +1,60 @@
 package Bc.영화표예매하기;
 
+import java.util.Scanner;
+
 public class MovieTicket {
     // 좌석 10개에 대한 정수형 배열의 인스턴스 변수 생성
-    int[] seat = new int[10];
+    private final int[] seat = new int[10];
     // 좌석당 가격에 대한 인스턴스 필드
-    int price;
+    private final int price;
+    // 입력을 받기 위한 스캐너 객체 생성
+    private final Scanner sc = new Scanner(System.in);
     // 생성자는 좌석당 가격 정보를 객체 생성 시 넣어야 되므로 매개변수가 있는 생성자 필요
-
+    public MovieTicket(int price) {
+        this.price = price;
+    }
     // 좌석 상태 표시 메서드 구현
-
+    public void printSeat() {
+        for(int e : seat) {
+            if(e == 0) System.out.print("[ ]");
+            else System.out.print("[V]");
+        }
+        System.out.println();
+    }
     // 좌석 예약 메서드 구현
-
+    public void selectSeat() {
+        printSeat();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("좌석 번호를 입력 하세요 : ");
+        int seatPos = sc.nextInt();
+        if (seat[seatPos - 1] == 0 ) {
+            seat[seatPos - 1] = 1;
+            System.out.println("좌석이 예매 되었습니다.");
+            printSeat();
+        } else {
+            System.out.println("이미 예약된 좌석 입니다.");
+        }
+    }
+    // 예약 취소하기 구현
+    public void cancelSeat() {
+        printSeat();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("취소할 좌석을 입력하세요 : ");
+        int seatPos = sc.nextInt();
+        if (seat[seatPos - 1] == 1) {
+            seat[seatPos - 1] = 0;
+            System.out.println("좌석 예약이 취소되었습니다.");
+            printSeat();
+        } else {
+            System.out.println("예약된 좌석이 아닙니다.");
+        }
+    }
     // 총 판매 금액 메서드 구현 (총 판매 금액은 인스턴스 필드로 만들지 말고 결과로 받아 반환)
-
-    private int;
-    private
+    public int totalAmount() {
+        int cnt = 0;
+        for (int e : seat) {
+            if (e == 1) cnt++;
+        }
+        return cnt * price;
+    }
 }
