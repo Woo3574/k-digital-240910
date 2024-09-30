a1f40ee7fccfe5429bb1295f249078221baaead5
Woo3574 <rlaxodn0429@gmail.com>
2024-09-30 14:48:18 +0900

    2024.09.30

--- src/Bq/번호판계산/NumberExi.java
+++ src/Bq/번호판계산/NumberExi.java
@@ -2,17 +2,17 @@ package Bq.번호판계산;
 
 import java.util.Scanner;
 
-public class HouseB {
+public class NumberExi {
     private int[] number = new int[100];
     private final Scanner sc = new Scanner(System.in);
-    private int[] lakeBoard;
-    public HouseB() {}
+    public NumberExi() {}
 
     public void setInput() {
         System.out.println("호수판 숫자 입력 : ");
-        lakeBoard = sc. // 150
+        String temporay = sc.next();
+        int[] lakeBoard = new int[temporay.length()];  // 150
         for (int i = 0; i < number.length ; i++) {
-            number[i] = lakeBoard(i) = '0' ;
+            number[i] = lakeBoard[i] - '0' ;
 
         }
     }
