f25a82028c8393f328d59e929f41bf6364202afd
Woo3574 <rlaxodn0429@gmail.com>
2024-09-30 12:51:39 +0900

    2024.09.30

---
+++ src/Bq/번호판계산/HouseB.java
@@ -0,0 +1,20 @@
+package Bq.번호판계산;
+
+import java.util.Scanner;
+
+public class HouseB {
+    private int[] number = new int[100];
+    private final Scanner sc = new Scanner(System.in);
+    private int[] lakeBoard;
+    public HouseB() {}
+
+    public void setInput() {
+        System.out.println("호수판 숫자 입력 : ");
+        lakeBoard = sc. // 150
+        for (int i = 0; i < number.length ; i++) {
+            number[i] = lakeBoard(i) = '0' ;
+
+        }
+    }
+
+}
