f4c5bbd1cfcd81b926d1659ff89bc4d2961750cb
Woo3574 <rlaxodn0429@gmail.com>
2024-10-03 23:58:00 +0900

    2024.10.03

--- src/Za/MakeACar/BmwXBus.java
+++ src/Za/MakeACar/BmwXBus.java
@@ -4,7 +4,7 @@ public class BmwXBus extends BmwXCar{
     int fuelEconomy = 5;
     double speed = 150;
     int fuelTankSize = 100;
-    int seatNumer = 20;
+    int seatNumber = 20;
 
     public BmwXBus(String name) {
         super(name);
@@ -13,8 +13,8 @@ public class BmwXBus extends BmwXCar{
     @Override
     double totalPrice(int nop, int area, int add_Ons) {
         double tp = 0;
-        double cnt = Math.ceil((double) nop / seatNumer);
-        if (nop < seatNumer) cnt = 1;
+        double cnt = Math.ceil((double) nop / seatNumber);
+        if (nop < seatNumber) cnt = 1;
         tp = cnt * DESTINATION_LIST[area] / fuelEconomy * oneLOil;
         return Math.floor(tp);
     }
@@ -22,8 +22,8 @@ public class BmwXBus extends BmwXCar{
     @Override
     double totalOilCnt(int nop, int area, int add_Ons) {
         double to = 0;
-        double cnt = Math.ceil((double) nop / seatNumer);
-        if (nop < seatNumer) cnt = 1;
+        double cnt = Math.ceil((double) nop / seatNumber);
+        if (nop < seatNumber) cnt = 1;
         if (ADD_ONS[add_Ons] == 1) {
             to = cnt * DESTINATION_LIST[area] / (fuelEconomy * (fuelTankSize + 30));
         } else to = cnt * DESTINATION_LIST[area] / (fuelEconomy * fuelTankSize);
@@ -33,8 +33,8 @@ public class BmwXBus extends BmwXCar{
     @Override
     double totalDistanceTime(int nop, int area, int weather, int add_Ons) {
         double tdt = 0;
-        double cnt = Math.ceil((double) nop / seatNumer);
-        if (nop < seatNumer) cnt = 1;
+        double cnt = Math.ceil((double) nop / seatNumber);
+        if (nop < seatNumber) cnt = 1;
         tdt = cnt * DESTINATION_LIST[area] / speed;
         if(weather == 2 || weather == 3) tdt *= WEATHER[weather];
         return tdt*60 ;
