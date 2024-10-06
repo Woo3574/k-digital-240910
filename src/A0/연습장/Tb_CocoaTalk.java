d4246cd7074ce10e97dec9b26be1b26a9c0ff8c9
Woo3574 <rlaxodn0429@gmail.com>
2024-10-06 15:55:35 +0900

    2024.10.06

--- src/A0/연습장/Tb_CocoaTalk.java
+++ src/A0/연습장/Tb_CocoaTalk.java
@@ -4,7 +4,7 @@ public class Tb_CocoaTalk {
     private String to; // 수신자
     private String msg; // 수신자에게 전달할 메세지
     public Tb_CocoaTalk(String name) {
-        to = name;
+        to = name; //
     }
 
     public void Write(String msg) {
