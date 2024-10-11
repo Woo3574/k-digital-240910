6c6490a1d9043724117526e5ac003e413fac5823
Woo3574 <rlaxodn0429@gmail.com>
2024-10-11 17:41:11 +0900

    2024.10.11

---
+++ src/자바파이널과제/JavaFinalReportMain.java
@@ -0,0 +1,15 @@
+package 자바파이널과제;
+
+// 임의의 위치에 텍스트 파일을 만들고, 10명의 정보를 공백 기준으로 미리 입력 해둠.
+// 이름 국어 영어 수학 형식
+
+// 해당 파일을 읽어 총점을 구하고
+// 총점이 높은 사람 순으로 이름과 총점 보여주기 (총점이 같은 경우 이름순)
+
+// 클래스하나를 만들어야한다
+
+public class JavaFinalReportMain {
+    public static void main(String[] args) {
+        String file =  "student.txt";
+    }
+}
---
+++ src/자바파이널과제/Student.java
@@ -0,0 +1,9 @@
+package 자바파이널과제;
+
+public class Student {
+
+    private String name;
+    private String kor;
+    private String eng;
+    private String mat;
+}
