90dc494bfcdcc31b949a5c3a0ed8cf2ca0aa7058
Woo3574 <rlaxodn0429@gmail.com>
2024-10-11 10:49:25 +0900

    2024.10.11

--- src/Dg/멀티스레드기본/ThreadBasicEx.java
+++ src/Dg/멀티스레드기본/ThreadBasicEx.java
@@ -9,6 +9,54 @@ package Dg.멀티스레드기본;
 
 public class ThreadBasicEx {
     public static void main(String[] args) {
+        Thread subThread1 = new CustomThread(); // Thread를 상속받은 객체 생성
+        Runnable runTask = new CustomRunnable(); // 작업 내용은 가지고 있지만 실제 쓰레드 동작은 하지 않는다
+        Thread subThread2 = new Thread(runTask); // 쓰레드 객체 생성
 
+        subThread1.start(); // run 으로 안쓰고 .start() 메서드 운영체제가 개입을 해주고있다
+        subThread2.start();
+
+        // 익명의 객체로 만드는 방법
+        Runnable task = new Runnable() {  // Runnable 인터페이스 문법
+            @Override
+            public void run() {
+                int sum = 0;
+                for (int i = 0; i <= 10; i++) {
+                    sum += i;
+                    System.out.println(Thread.currentThread() + " : " + sum);
+                }
+                System.out.println(Thread.currentThread() + "합계 : " + sum);
+            }
+        };
+        Thread subThread3 = new Thread(task);
+        subThread3.start();
+    }
+}
+
+
+// Thread 클래스를 상속 받음
+class CustomThread extends Thread {
+    @Override
+    public void run() { // run 운영체제가 개입을 해주고있다
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum += i;
+            System.out.println(Thread.currentThread() + " : " + sum);
+        }
+        System.out.println(Thread.currentThread() + "합계 : " + sum); // currentThread 지금 쓰레드가 누군지 알려주는 메서드
+    }
+
+}
+
+// Runnable 인터페이스 구현
+class CustomRunnable implements Runnable {
+    @Override
+    public void run() {
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum += i;
+            System.out.println(Thread.currentThread() + " : " + sum);
+        }
+        System.out.println(Thread.currentThread() + "합계 : " + sum);
     }
 }
