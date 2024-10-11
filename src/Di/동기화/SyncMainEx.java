adc092630dc4482c694645ee4439475af4e43a05
Woo3574 <rlaxodn0429@gmail.com>
2024-10-11 11:48:22 +0900

    2024.10.11

---
+++ src/Di/동기화/SyncMainEx.java
@@ -0,0 +1,33 @@
+package Di.동기화;
+
+// 동기화(Synchronized) : 한번에 한개의 스레드만 공유 자원에 접근 할 수 있도록,
+// 락(Lock)을 걸어서 다른 스레드가 진행중인 작업을 간섭하지 못하도록 하는 것
+
+import static java.lang.Thread.sleep;
+
+public class SyncMainEx {
+    public static void main(String[] args) {
+        Thread  thread = new Thread(() -> {
+
+        });
+    }
+}
+
+class shareThread {
+    private int value = 0;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        try {
+            sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getName() +
+                "의 Value 값은 " + this.value + "입니다.");
+    }
+}
