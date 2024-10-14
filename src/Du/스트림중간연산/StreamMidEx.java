e86cc98474f12bdb95ea52262b15e16b60a5f909
Woo3574 <rlaxodn0429@gmail.com>
2024-10-14 15:48:26 +0900

    2024.10.14

--- src/Du/스트림중간연산/StreamMidEx.java
+++ src/Du/스트림중간연산/StreamMidEx.java
@@ -7,6 +7,7 @@ package Du.스트림중간연산;
 // 정렬 : sorted()
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -16,19 +17,49 @@ public class StreamMidEx {
         IntStream stream2 = IntStream.of(7, 5, 5, 2, 1, 2, 3, 4, 6);
         // 중복 제거 이후 결과 출력
         stream1.distinct().forEach(e -> System.out.print(e + " "));
-
         System.out.println();
+
         // 필터링이후 결과 출력
         stream2.filter(e -> e % 2 != 0).forEach(e-> System.out.print(e + " "));
-
         System.out.println();
+
         // 스트림 변환 : map(), flatMap()
         Stream<String> stream = Stream.of("HTML", "CSS", "JAVA", "REACT");
         stream.map(e -> e.length()).forEach(e -> System.out.print(e + " "));
-
         System.out.println();
+
         String[] arr = {"I study hard", "You Study JAVA", "I am hungry"};
         Stream<String> stream3 = Arrays.stream(arr);
         stream3.flatMap(s -> Stream.of(s.split(" "))).forEach(s -> System.out.print(s + "/"));
+        System.out.println();
+
+        // 중개 연산 : 제한
+        IntStream stream4 = IntStream.range(0, 10); // 0 ~ 10 미만
+        IntStream stream5 = IntStream.range(0, 10);
+        IntStream stream6 = IntStream.range(0, 10);
+
+        // skip() : 전달된 개수만큼의 요소를 제외하고 스트림 반환
+        stream4.skip(4).forEach(e -> System.out.print(e + " "));
+        System.out.println();
+
+        // limit() : 전달된 개수만큼의 요소로 이루어진 새로운 스트림 반환
+        stream5.limit(5).forEach(e -> System.out.print(e + " "));
+        System.out.println();
+
+        // sorted() : comparator를 이용해 정렬
+        Stream<String> stream7 = Stream.of("JAVA", "HTML", "JAVASCRIPT", "CSS");
+        Stream<String> stream8 = Stream.of("JAVA", "HTML", "JAVASCRIPT", "CSS");
+        Stream<String> stream9 = Stream.of("JAVA", "HTML", "JAVASCRIPT", "CSS");
+        // 오름차순
+        stream7.sorted().forEach(e -> System.out.print(e + " "));
+        System.out.println();
+        // 내림차순
+        stream8.sorted(Comparator.reverseOrder()).forEach(e -> System.out.print(e + " "));
+        System.out.println();
+        // 길이 기준으로 정렬하고, 길이가 같으면 알파벳 정렬
+        stream9.sorted(Comparator // 중개 연산안에 조건을 2개 사용
+                .comparingInt(String::length)
+                .thenComparing(String::toString))
+                .forEach(e -> System.out.print(e + " "));
     }
 }
