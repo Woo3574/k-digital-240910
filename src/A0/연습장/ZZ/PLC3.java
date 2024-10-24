d4589776d674864a1969cec6b73fea2207bc6bc4
Woo3574 <rlaxodn0429@gmail.com>
2024-10-24 20:55:08 +0900

    2024.10.24

---
+++ src/A0/연습장/ZZ/PLC3.java
@@ -0,0 +1,60 @@
+package A0.연습장.ZZ;
+
+public class PLC3 {
+//
+//    package VO;
+//
+//    public class StoreVO {
+//        private String storeId;
+//        private int sales; //
+//        private int capital;
+//
+//        public StoreVO(String storeId,int sales,int capital) {
+//            this.storeId = storeId;
+//            this.sales = sales;
+//            this.capital = capital;
+//        }
+//
+//        public StoreVO(String storeId, int capital) {
+//            this.storeId = storeId;
+//            this.capital = capital;
+//        }
+//
+//        public StoreVO(int sales) {
+//            this.sales = sales;
+//        }
+//
+//        public StoreVO(String storeId) {
+//            this.storeId = storeId;
+//        }
+//
+//
+//        public String getStoreId() {
+//            return storeId;
+//        }
+//
+//        public int getSales() {
+//            return sales;
+//        }
+//
+//        public int getCapital() {
+//            return capital;
+//        }
+//
+//        public void setStoreId(String storeId) {
+//            this.storeId = storeId;
+//        }
+//
+//        public void setSales(int sales) {
+//            this.sales = sales;
+//        }
+//
+//        public void setCapital(int capital) {
+//            this.capital = capital;
+//        }
+//    }
+//
+//
+
+
+}
